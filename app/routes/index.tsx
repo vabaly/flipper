6fa3d9f60570dbca9488d27d7ed78aac3b224ac2
vabaly <love0and0life@gmail.com>
2023-01-06 17:11:29 +0800

    feat: add index to flipper

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,29 @@
+import { Link } from '@remix-run/react'
+import {ListItem, ListItemLabel} from 'baseui/list'
+// import {StyledLink} from 'baseui/link'
+
+const LINKS: [string, string][] = [
+  ['/flipper', 'Flipper'],
+]
+
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+      <ol>
+        {LINKS.map(([href, label], index) => (
+          <ListItem
+            key={label}
+            artwork={() => <div>{index + 1}</div>}
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+            <ListItemLabel>
+              <Link to={href}>
+                {/* <StyledLink>{label}</StyledLink> */}
+                {label}
+              </Link>
+            </ListItemLabel>
+          </ListItem>
+        ))}
+      </ol>
     </div>
   )
 }
